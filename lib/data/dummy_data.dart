c05efda96daf7307d1a6793dd894171de04a6dbb
aditya kumar <akv.small.api@gmail.com>
2023-11-09 19:18:23 +0530

    added grid view for various food items

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+const availableCategories = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Maxican',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Indian',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Russian',
+    color: Colors.green,
+  )
+];
