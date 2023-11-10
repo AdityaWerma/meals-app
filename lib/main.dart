694cd7117009f259f773de9ae1269e16119c03a0
aditya kumar <akv.small.api@gmail.com>
2023-11-10 11:50:01 +0530

    added navigator to push from one screen to another

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const MealsScreen(title: 'daf', meals: dummyMeals),
+      home: const CategoriesScreen(),
     );
   }
 }
